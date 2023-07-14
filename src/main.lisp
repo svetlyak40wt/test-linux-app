19b32dcb18a9feb734d184eebfbe3cf00d88a888
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-07-14 23:19:55 +0300

    Commented osicat.

--- src/main.lisp
+++ src/main.lisp
@@ -1,15 +1,18 @@
 (uiop:define-package #:test-brew-app/main
   (:use #:cl)
-  (:import-from #:osicat))
+  ;; (:import-from #:osicat)
+  )
 (in-package #:test-brew-app/main)
 
 
 (defun main ()
-  (format t "My pid is: ~A~%"
-          (osicat-posix:getpid))
-  (format t "Here are all env vars:~%")
-  (loop for (key . value) in (sort (osicat:environment)
-                                   #'string<
-                                   :key #'car)
-        do (format t "~A = ~A~%"
-                   key value)))
+  (format t "Hello world!~%")
+  ;; (format t "My pid is: ~A~%"
+  ;;         (osicat-posix:getpid))
+  ;; (format t "Here are all env vars:~%")
+  ;; (loop for (key . value) in (sort (osicat:environment)
+  ;;                                  #'string<
+  ;;                                  :key #'car)
+  ;;       do (format t "~A = ~A~%"
+  ;;                  key value))
+  )
